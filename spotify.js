b4cab7488c0f0ad7defc8990c9b40fc99ef35efd
Jean-Luc19 <aticus19@gmail.com>
2017-02-14 14:13:46 -0700

    get artist function is working

--- spotify.js
+++ spotify.js
@@ -1,28 +1,30 @@
  const getFromApi = function (endpoint, query) {
-  const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
-  Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
-  return fetch(url).then(function (response) {
-    if (!response.ok) {
-      return Promise.reject(response.statusText);
-    }
-    return response.json();
-  });
-};
+   const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
+   Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+   return fetch(url).then(function (response) {
+     if (!response.ok) {
+       return Promise.reject(response.statusText);
+     }
+     return response.json();
+   });
+ };
 
-const artist =  item.artists.items[0];
-const getArtist = function (name) {
-//Make a call to the search endpoint using the getFromApi function.
-  const query = {
-    q: name,
-    limit: 1,
-    type: 'artist'
-  }
-  getFromApi('search', query).then (response =>{
-    const artist = response.artists.items[0].name;
-    return artist;
-  })
-};
 
+ const getArtist = function (name) {
+   //Make a call to the search endpoint using the getFromApi function.
+   const query = {
+     q: name,
+     limit: 1,
+     type: 'artist'
+   }
+   let endpoint = 'search';
+   return getFromApi(endpoint, query).then(response => {
+     let artist = response.artists.items[0];
+     return artist;
+
+   })
+
+ };
 //Use .then to add a callback which will run when getFromApi resolves.
 
 
